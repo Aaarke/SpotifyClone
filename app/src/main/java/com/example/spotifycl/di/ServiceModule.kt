a621f40caef15f4d9a794e31cfe481aac82058d8
roshan kumar <roshankumarroshi@gmail.com>
2020-11-30 00:23:12 +0530

    service and adpter added

--- app/src/main/java/com/example/spotifycl/di/ServiceModule.kt
+++ app/src/main/java/com/example/spotifycl/di/ServiceModule.kt
@@ -1,5 +1,6 @@
 package com.example.spotifycl.di
 
+import android.app.Application
 import android.content.Context
 import com.example.spotifycl.exoplayer.FirebaseMusicSource
 import com.example.spotifycl.remote.MusicDataBase
@@ -11,11 +12,12 @@ import com.google.android.exoplayer2.util.Util
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ServiceScoped
 
 @Module
-@InstallIn(ServiceModule::class)
+@InstallIn(ServiceComponent::class)
 object ServiceModule {
 
     @ServiceScoped
